2758d97cb932822c67133f94a8f2ad936cc70511
marcoswmv <marcoswmv@gmail.com>
2020-10-04 02:06:11 +0100

    Initial Commit

---
+++ TwitchTopGames/TwitchTopGamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitchTopGamesApp.swift
+//  TwitchTopGames
+//
+//  Created by Marcos Vicente on 04.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct TwitchTopGamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
